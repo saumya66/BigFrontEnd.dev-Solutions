77cf24281ac3af0c4a28efeac5c789cedf2ac9ad
Saumya <60464414+saumya66@users.noreply.github.com>
2023-01-26 23:27:43 +0530

    Create ImplementRange().js

---
+++ ImplementRange().js
@@ -0,0 +1,7 @@
+function range(from, to) {
+  let arr = []
+  for(let i = from ;i<=to;i++){ //just iterating from starting to end number and pusing all the numbers from the starting point to the endpoint point into the arr
+    arr.push(i)
+  }
+  return arr
+}
